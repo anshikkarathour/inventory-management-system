320eb3a0ccc7ee537367e11b9488ed01b9109378
Anshika <anshikarathour04@gmail.com>
2026-06-03 17:09:49 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,9 @@ from app.routes.order import router as order_router
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "https://inventory-management-system-fztq.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
