6d05a46cf3dd9ee89a922b7e33963609f75e8897
Anshika <anshikarathour04@gmail.com>
2026-06-03 17:23:36 +0530

    update CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,10 +11,14 @@ from app.models.order_item import OrderItem
 from app.routes.product import router as product_router
 from app.routes.customer import router as customer_router
 from app.routes.order import router as order_router
+
 app = FastAPI()
+
+# CORS (production + local)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:3000",
         "https://inventory-management-system-fztq.vercel.app",
     ],
     allow_credentials=True,
@@ -22,13 +26,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Create DB tables
 Base.metadata.create_all(bind=engine)
 
+# Routes
 app.include_router(product_router)
 app.include_router(customer_router)
 app.include_router(order_router)
 
-
+# Health check route
 @app.get("/")
 def home():
     return {"message": "Inventory API Running"}
